273359fe203eab0897d25861ba9908db9996674e
Jonathan Rose <jonr87ee@gmail.com>
2015-08-15 18:06:32 -0400

    add list feature with working filters

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  namespace :api, path: '/', constraints: {subdomain: 'api'} do
+    resources :appointments
+  end
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
---
+++ test/integration/listing_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class ListingTest < ActionDispatch::IntegrationTest
+  # setup code here
+  setup { host! 'api.example.com'}
+
+  test 'returns a list of appointments' do
+    get '/appointments'
+    assert_equal 200, response.status
+    refute_empty response.body
+  end
+
+  test 'filter by start and end time' do
+    jonApt = Appointment.create!(first_name: 'Jon', last_name: 'Rose', start_time: '12/1/2015 07:00AM', end_time: '12/1/2015 08:00AM')
+    get '/appointments?first_name=Jon'
+    assert_equal 200, response.status
+    appointments = JSON.parse(response.body)
+    names = appointments.collect { |a| a['first_name'] }
+    assert_includes names, 'Jon'
+
+  end
+
+end
